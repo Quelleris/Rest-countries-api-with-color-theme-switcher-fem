1abd97302420a494c4fbc4a5f3c84e4973c2facf
Maja Frydrych <maja.frydrych@gmail.com>
2023-08-09 15:28:03 +0200

    Add font weight to country info item

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -4,20 +4,23 @@ import { Link } from 'react-router-dom'
 export default function Country({image, name, population, region, capital}) {
   return (
     <div className="country-component">
-        <img src={image} alt={name} />
+        <img src={image} alt={name} className='country-component-image'/>
         <div className="country-content">
             <h2 className="coutry-title">
                 <Link to={`/${name}`}>{name}</Link>
                 </h2>
             <ul className="coutry-info-list">
                 <li className="country-info-item">
-                    Population: {population.toLocaleString()}
+                    <span className='fw-regular'>Population: </span>
+                    {population.toLocaleString()}
                 </li>
                 <li className="country-info-item">
-                    Region: {region}
+                    <span className='fw-regular'>Region: </span> 
+                    {region}
                 </li>
                 <li className="country-info-item">
-                    Capital: {capital}
+                    <span className='fw-regular'>Capital: </span>
+                    {capital}
                 </li>
             </ul>
         </div>
