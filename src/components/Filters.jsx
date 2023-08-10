b0dcf47e805af9e503b88da2ea40c2a2dfd98c02
Maja Frydrych <maja.frydrych@gmail.com>
2023-08-10 14:11:51 +0200

    add search filter

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import {FaSearch} from 'react-icons/fa'
 
-export default function Filters() {
+export default function Filters({onNameSearch, onRegionSearch}) {
+
   return (
-    <div>Filters</div>
+    <div className="filter-section">
+      <div className="container">
+        <div className="input-wrapper">
+        <input onChange={e => onNameSearch(e.target.value)} type="search" className='country-filter-input filter-component' placeholder='Search for a country...'/>
+        <FaSearch className='search-icon'/>
+        </div>
+        <div className="select-wrapper">
+          <label htmlFor="region-select"></label>
+          <select onChange={e => onRegionSearch(e.target.value)} name="regions" id="region-select" className='region-select filter-component'>
+            <option value="">Filter by Region</option>
+            <option value="Africa">Africa</option>
+            <option value="America">America</option>
+            <option value="Asia">Asia</option>
+            <option value="Europe">Europe</option>
+            <option value="Oceania">Oceania</option>
+          </select>
+        </div>
+        
+      </div>
+    </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ import CountryList from './CountryList'
 
 export default function Home() {
   const [countries, setCountries] = useState(null);
+  const [filteredCountries, setFilteredCountries] = useState(null)
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all?fields=capital,population,name,region,flags")
@@ -20,12 +21,39 @@ export default function Home() {
         }))
       })
   }, [])
+
+  function filterCountriesByName(searchInput) {
+    if (searchInput !== "") {
+      const filteredData = countries.filter((country) => {
+        return country.name.toLowerCase().includes(searchInput.toLowerCase())
+      })
+      setFilteredCountries(filteredData)
+    }
+    else {
+      setFilteredCountries(null)
+    }
+  }
+
+  function filterCountriesByRegion(option) {
+    if (option !== "") {
+      const filteredData = countries.filter((country) => {
+        return country.region.includes(option)
+      })
+      setFilteredCountries(filteredData)
+    }
+    else {
+      setFilteredCountries(null)
+    }
+  }
+
   return (
     <main className='main'>
-    <Filters />
+    <Filters 
+    onNameSearch={filterCountriesByName}
+    onRegionSearch={filterCountriesByRegion}/>
     <section className="countries-section">
       <div className="container">
-        {countries && <CountryList countries={countries}/>}
+        {countries && <CountryList countries={filteredCountries ? filteredCountries : countries}/>}
       </div>
     </section>
   </main>
