440a6e634c4d5e25bb3ada73b381686ff470737c
Maja Frydrych <maja.frydrych@gmail.com>
2023-08-13 20:12:23 +0200

    add sun icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import  {FaMoon} from 'react-icons/fa';
+import React, { useEffect, useState } from "react";
+import { FaMoon } from "react-icons/fa";
+import { BsFillSunFill } from "react-icons/bs";
 
 export default function Header() {
-  const [darkMode, setDarkMode] = useState(false);
+	const [darkMode, setDarkMode] = useState(false);
 
-  useEffect(() => {
-    if (darkMode) {
-      document.body.classList.add("dark");
-    } else {
-      document.body.classList.remove("dark");
-    }
-  }, [darkMode])
+	useEffect(() => {
+		if (darkMode) {
+			document.body.classList.add("dark");
+		} else {
+			document.body.classList.remove("dark");
+		}
+	}, [darkMode]);
 
-  return (
-    <header className='header'>
-        <div className="container">
-            <h1 className="header-title">Where in the world?</h1>
-            <button onClick={() => setDarkMode(!darkMode)} type='button' className='theme-switch-btn'>
-              <FaMoon/>
-              <span>{darkMode ? 'Dark ' : "Light "} Mode</span>
-            </button>
-        </div>
-    </header>
-  )
+	return (
+		<header className='header'>
+			<div className='container'>
+				<h1 className='header-title'>Where in the world?</h1>
+				<button
+					onClick={() => setDarkMode(!darkMode)}
+					type='button'
+					className='theme-switch-btn'
+				>
+					{darkMode ? <FaMoon /> : <BsFillSunFill />}
+					<span>{darkMode ? "Dark " : "Light "} Mode</span>
+				</button>
+			</div>
+		</header>
+	);
 }
